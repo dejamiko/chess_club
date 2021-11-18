d183815dc194985a9c7151ea700955d21d65284a
Zafi Shah <shah.zafira@gmail.com>
2021-11-18 19:36:00 +0000

    added profile page
    Added profile page, base and base_content. Can't really test till login and users are created

--- clubs/views.py
+++ clubs/views.py
@@ -1,3 +1,29 @@
+# from clucker
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.hashers import check_password
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseForbidden
+from django.shortcuts import redirect, render
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
+
+#@login_prohibited
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
+def profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = UserForm(instance=current_user, data=request.POST)
+        if form.is_valid():
+            messages.add_message(request, messages.SUCCESS, "Profile updated!")
+            form.save()
+            return redirect('feed')
+    else:
+        form = UserForm(instance=current_user)
+    return render(request, 'profile.html', {'form': form})
