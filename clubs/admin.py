3e2c41b3a610bfa27c70ca9d6e19b76fa99330b2
Zafi Shah <shah.zafira@gmail.com>
2021-12-10 16:31:15 +0000

    Added test cases

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Tournament, Club
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "email", "chess_exp"]
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'name'
+    ]
+
+@admin.register(Tournament)
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'club', 'name'
+    ]
